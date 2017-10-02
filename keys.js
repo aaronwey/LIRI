35ed57da6cc44f871e65e862b790c6ed4b57879a
aaronwey <aaronwey@gmail.com>
2017-10-01 19:43:34 -0600

    2nd commit

---
+++ keys.js
@@ -0,0 +1,12 @@
+// console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'GRSKLFXbkN6LHi9tHtukLDrFs',
+  consumer_secret: 'f3bvgoUOBWi0Ez1VJIMA5fwg2075m5fVU4BzOtoCGGUGa0AH0y',
+  access_token_key: '50693061-2Tle7s8DSTLuRa4yeimKUKUKPuYppYzhG2ThkttGz',
+  access_token_secret: '5pV9qfETP0Bi5lNxFGFVPOau8pWCZow9Wu6AJal9GztUA',
+}
+
+
+
+module.exports = twitterKeys;
